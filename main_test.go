cc29c4477ec065ea9d1ddc628e20efadfcce4d17
Manfred Touron <94029+moul@users.noreply.github.com>
2021-03-28 21:33:14 +0000

    chore: unit test the CLI + update the README
    Signed-off-by: Manfred Touron <94029+moul@users.noreply.github.com>

--- main_test.go
+++ main_test.go
@@ -1,20 +1,25 @@
 package main
 
 import (
+	"bytes"
 	"testing"
 
 	"go.uber.org/goleak"
 )
 
-func TestRun(t *testing.T) {
-	/*
-		err := run(nil)
-		if err != nil {
-			t.Fatalf("err should be nil: %v", err)
-		}
-	*/
-}
-
 func TestMain(m *testing.M) {
 	goleak.VerifyTestMain(m)
+}
+
+func Example() {
+	var stdin bytes.Buffer
+	stdin.Write([]byte("7\n"))
+	err := run([]string{"captcha", "-seed=42"}, &stdin)
+	if err != nil {
+		panic(err)
+	}
+
+	// Output:
+	// 3 + 4
+	// ->
 }
